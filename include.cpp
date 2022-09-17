03462e980e48b6675ce0479398e15e8067eb6b8c
TsotneSV <112477768+TsotneSV@users.noreply.github.com>
2022-09-17 16:12:56 +0400

    Update and rename template.cpp to include.cpp

--- include.cpp
+++ include.cpp
@@ -1,6 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
 #define int long long
 #define f first
 #define s second
@@ -9,12 +8,13 @@ using namespace std;
 #define all(x) (x).begin(),(x).end()
 #define rall(x) (x).rbegin(),(x).rend()
 #define deb(x) cout<<(x)<<"\n"
+#define deq(x) cout<<(x)<<" ";
 #define fast_cin() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 const int INF=1e9+7,MOD=998244353/*1e9+7*/;
 
 int tt=1;
 void solve(){
-
+    
 }
  
 signed main(){
