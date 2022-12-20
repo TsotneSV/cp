44911b0a6c17a9a018fc332a70c5d97fad977e58
TsotneSV <112477768+TsotneSV@users.noreply.github.com>
2022-12-20 22:42:11 +0400

    Update include.cpp

--- include.cpp
+++ include.cpp
@@ -1,27 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
- 
+
 #define ll long long
-#define ld long double
-#define f first
-#define s second
-#define mp make_pair
 #define pb push_back
 #define all(x) (x).begin(),(x).end()
-#define rall(x) (x).rbegin(),(x).rend()
 #define deb(x) cout<<(x)<<"\n"
-#define fast_cin() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 const int INF=1e9,MOD=1e9+7;
 
-int tt=1;
+
+
+int tt=1,;
 void solve(){
-    
+	
 }
- 
+
 main(){
-    fast_cin();
-    //cin>>tt;
+    ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    cin>>tt;
     while(tt--){
         solve();
     }
 }
+
